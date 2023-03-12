cb382c79013afe05c8d7a1c83f0806761b8f7c8a
Thileepan-Ilankumaran <thileep.ilankumaran@gmail.com>
2023-03-13 01:51:18 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends  Component {
+  render(){
+  return (
+    <div className="App">
+      <h1>Hi, I'm React Apps</h1>
+      <p>This is really working</p>
+      <Person />
+      <Person />
+      <Person />
+    </div>
+  );
+  return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does it work now?'))
+  }
+}
+
+export default App;
