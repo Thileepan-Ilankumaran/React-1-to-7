ada0cb1410c0e1bf3a35121e8ca24a6400e395b9
Thileepan-Ilankumaran <thileep.ilankumaran@gmail.com>
2023-03-13 11:01:55 +0530

    Apps to App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends  Component {
   render(){
   return (
     <div className="App">
-      <h1>Hi, I'm React Apps</h1>
+      <h1>Hi, I'm React App</h1>
       <p>This is really working</p>
       <Person />
       <Person />
